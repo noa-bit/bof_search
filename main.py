f72cb581ca54b8b63321766f9b94aa1f3262b14e
noa <noa.carlberg@gmail.com>
2026-03-01 17:27:08 +0100

    repo work

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import requests, json
-from shapely import Point, Polygon
+from requests import get
+from json import dump, load, decoder
 from webhook import webhook
+from dotenv import load_dotenv
+import os
 
 def desirable(size, price, student):
     if student == True and price <= 6097 and size >= 23:
@@ -8,18 +10,19 @@ def desirable(size, price, student):
     else:
         return False
 
-url = "https://bostad.stockholm.se/AllaAnnonser/"
-r = requests.get(url)
+load_dotenv()
+url = os.getenv("bf_url")
+r = get(url)
 data = r.json()
 
 with open("data.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, indent=4, ensure_ascii=False)
+    dump(data, f, indent=4, ensure_ascii=False)
 
 try:
     with open("prospects.json", "r", encoding="utf-8") as f:
-        prospects = json.load(f)
+        prospects = load(f)
         # print(prospects)
-except json.decoder.JSONDecodeError:
+except decoder.JSONDecodeError:
     prospects = []
 
 def main():
@@ -39,10 +42,10 @@ def main():
             if obj not in prospects:
                 if desirable(size, price, student_type):
                     # Skicka notis i discord embed
-                    webhook(title=f"{where}, {address}", description="Ny annons hittad.",url=f"https://bostad.stockholm.se/bostad/{id}")
+                    webhook(title=f"{where}, {address}", description=f"Ny annons hittad. Pris: {price} kr. Storlek: {size} kvm. Annonseras till {date}.", url=f"https://bostad.stockholm.se/bostad/{id}")
                     prospects.append(obj)
                     with open("prospects.json", "w", encoding="utf-8") as f:
-                        json.dump(prospects, f, indent=4, ensure_ascii=False)
+                        dump(prospects, f, indent=4, ensure_ascii=False)
         except Exception as error:
             print(f"Error processing object: {error}")
 
